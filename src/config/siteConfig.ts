5fcc75ca9bff53831ba077d0bc69a92b564ae689
LuoYu0712 <liaozihe48@outlook.com>
2026-07-31 15:37:11 +0800

    config: 修改部分配置

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -9,10 +9,10 @@ export const siteConfig: SiteConfig = {
 	title: "纸鹤の小窝",
 
 	// 站点副标题
-	subtitle: "纸鹤的博客",
+	subtitle: "φ(゜▽゜*)♪",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://blog.CraneOvO.top",
 
 	// 站点描述
 	description:
@@ -89,7 +89,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2026-07-30",
+	siteStartDate: "2026-07-31",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
